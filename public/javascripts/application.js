d625d0990f575909ba2d77ccfbc4c4349697a1de
Ian Greulich <ian.greulich@gmail.com>
2012-02-23 15:14:46 -0500

    added logic to display current Kardashian, next Kardashian, and the number of days until your next

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,7 +7,13 @@ $(function(){
 $(function(){
 	$('#datepicker').change(function(){
 		var start_date = new Date($('#datepicker').val());
-		
-		alert(start_date);
+		var diff =  Math.floor((new Date() - start_date) / 86400000);
+		var days_until_kanniv = diff % KARDASHIAN;
+		var kanniv = trunc(diff / KARDASHIAN);
+		alert("You have been together for " + kanniv + " Kardashians.\nYou have " + days_until_kanniv + " days until your " + (kanniv + 1) + " Kardashian anniversary.");
 	});
 });
+
+function trunc(n){
+   return ~~n;
+}
