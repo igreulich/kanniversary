0dc02a28122076bffe01a23f4b1a155f9833a3ce
Ian Greulich <ian.greulich@gmail.com>
2012-02-23 12:24:18 -0500

    added datepicker, and alert box'

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,13 @@
+var KARDASHIAN = 72;
+
 $(function(){
 	$("#datepicker").datepicker({maxDate: "+0D" });
 });
 
 $(function(){
-	
+	$('#datepicker').change(function(){
+		var start_date = new Date($('#datepicker').val());
+		
+		alert(start_date);
+	});
 });
