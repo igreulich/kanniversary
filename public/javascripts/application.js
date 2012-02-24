583030c99941f7b746f43a01a9a821cad79b5052
Ian Greulich <ian.greulich@gmail.com>
2012-02-24 16:23:43 -0500

    refactored application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
-var KARDASHIAN = 72;
-var TODAY      = new Date();
 var MONTHS     = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 var DAYS       = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var TODAY      = new Date();
+var KARDASHIAN = 72;
 
 function trunc(n){
   return ~~n;
@@ -15,27 +15,34 @@ function month(n){
   return MONTHS[n];
 }
 
+function next_kanniv_date(n){
+  var next_kanniv_day   = n.getDay();
+  var next_kanniv_month = n.getMonth();
+  var next_kanniv_date  = n.getDate();
+
+  return day(next_kanniv_day) + ', ' + month(next_kanniv_month) + ' ' + next_kanniv_date;
+}
+
+function diff_in_days(n){
+  return Math.floor((TODAY - n) / 86400000);
+}
+
+var find_the_kardashian = function(){
+  var start_date        = new Date($('#datepicker').val());
+  var days_until_kanniv = diff_in_days(start_date) % KARDASHIAN;
+  var kanniv            = trunc(diff_in_days(start_date) / KARDASHIAN);
+  var next_kanniv       = TODAY.advance({ day: days_until_kanniv });
+  
+  var line1 = "You have been together for " + kanniv + " Kardashians.\n\n";
+  var line2 = "Your " + (kanniv + 1) + " Kardashian anniversary is in " + days_until_kanniv + " days, on " + next_kanniv_date(next_kanniv) +  ".";
+  
+  alert(line1 + line2);
+}
+
 $(function(){
   $("#datepicker").datepicker({maxDate: "+0D" });
 });
 
 $(function(){
-  $('#datepicker').change(function(){
-    var start_date        = new Date($('#datepicker').val());
-    var diff              =  Math.floor((TODAY - start_date) / 86400000);
-    var days_until_kanniv = diff % KARDASHIAN;
-    var kanniv            = trunc(diff / KARDASHIAN);
-    var next_kanniv       = TODAY.advance({ day: days_until_kanniv });
-    
-    var next_kanniv_day   = next_kanniv.getDay();
-    var next_kanniv_month = next_kanniv.getMonth();
-    var next_kanniv_date  = next_kanniv.getDate();
-    
-    var kanniv_date_pretty = day(next_kanniv_day) + ', ' + month(next_kanniv_month) + ' ' + next_kanniv_date;
-    
-    var line1 = "You have been together for " + kanniv + " Kardashians.\n\n";
-    var line2 = "Your " + (kanniv + 1) + " Kardashian anniversary is in " + days_until_kanniv + " days, on " + kanniv_date_pretty +  ".\n\n";
-    
-    alert(line1 + line2);
-  });
+  $('#datepicker').change(find_the_kardashian);
 });
